f413e2a97b6810f3b7ff92fc0c701de9d0776ad9
dalloliogm <dalloliogm@gmail.com>
2008-11-19 09:48:38 +0100

    Initial commit. Added open_big_file.py

---
+++ open_big_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Script to open portions of big files.
+# In particular, this script has been designed to open big output files from sequencing projects or microarrays.
+# If the file has more than 1000 lines, only the first 1000 are showed
+# If the file has more than 100 columns, only the first 100 are showed.
+
+gedit $1
