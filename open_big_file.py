6bd4206d240e7da9bfa9c80366c26cacd6257d47
dalloliogm <dalloliogm@gmail.com>
2008-11-19 14:41:16 +0100

    refactored, added a openbigfiles function

--- open_big_file.py
+++ open_big_file.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
-# Script to open portions of big files.
-# In particular, this script has been designed to open big output files from sequencing projects or microarrays.
-# If the file has more than 1000 lines, only the first 1000 are showed
-# If the file has more than 18 columns or 1000 bytes, cut it
+"""
+Script to open portions of big files.
+In particular, this script has been designed to open big output files from sequencing projects or microarrays.
+If the file has more than 1000 lines, only the first 1000 are showed
+If the file has more than 18 columns or 1000 bytes, cut it
+
+>>> os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'] = '/proc/cpuinfo\n/proc/meminfo'
+
+"""
+
 
 import sys
 import os
 
-filename = sys.argv
-tmpfile = file('/tmp/cuttedfile', 'w')
+def openfiles():
+    output = ''
+    for f in os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'].split('\n'):
+        tmpfile = file('/tmp/cuttedfile', 'w')
+        output += f
+        tmpfile.write(output)
+        os.popen('gedit /tmp/cuttedfile &')
 
-
-output = ''
-
-for f in os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'].split('\n'):
-    output += f
-
-tmpfile.write(output)
-os.popen('gedit /tmp/cuttedfile &')
 
 
 
