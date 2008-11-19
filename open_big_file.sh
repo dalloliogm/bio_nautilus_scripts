00bc86cedf5556681954967079d24caa99fd3d1f
dalloliogm <dalloliogm@gmail.com>
2008-11-19 10:34:02 +0100

    switched to a bash script with cut and head

--- open_big_file.sh
+++ open_big_file.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env bash
 # Script to open portions of big files.
 # In particular, this script has been designed to open big output files from sequencing projects or microarrays.
 # If the file has more than 1000 lines, only the first 1000 are showed
-# If the file has more than 100 columns, only the first 100 are showed.
+# If the file has more than 18 columns or 1000 bytes, cut it
 
-gedit $1
+head -n 1000 $1 |cut -f 1-18|cut -b 1000
