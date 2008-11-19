e257adc0c4b4e6f192cc47f886782733cd953ae1
dalloliogm <dalloliogm@gmail.com>
2008-11-19 14:39:43 +0100

    added doc and a cycle over files in open_big_file.py

--- open_big_file.py
+++ open_big_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# Script to open portions of big files.
+# In particular, this script has been designed to open big output files from sequencing projects or microarrays.
+# If the file has more than 1000 lines, only the first 1000 are showed
+# If the file has more than 18 columns or 1000 bytes, cut it
 
 import sys
 import os
@@ -6,8 +10,16 @@ import os
 filename = sys.argv
 tmpfile = file('/tmp/cuttedfile', 'w')
 
-tmpfile.write(str(filename) + '\n')
-tmpfile.write(str(dir()) + '\n')
-tmpfile.write(str(os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS']) + '\n')
 
+output = ''
+
+for f in os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'].split('\n'):
+    output += f
+
+tmpfile.write(output)
 os.popen('gedit /tmp/cuttedfile &')
+
+
+
+if __name__ == '__main__':
+    os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'] = '/proc/cpuinfo\n/proc/meminfo'
